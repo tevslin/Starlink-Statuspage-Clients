80977bf0f7c2bfaaee47eeddb71468c1b1e9f24f
Tom Evslin <tom@evslin.com>
2021-10-08 14:55:26 -0400

    Create ScheduleTask.ps1

---
+++ ScheduleTask.ps1
@@ -0,0 +1,2 @@
+﻿$StarlinkFolder="C:\users\$env:USERNAME\documents\StarlinkScripts"
+schtasks /create /sc minute /mo 15 /tn testy /tr "powershell.exe -windowstyle hidden $StarlinkFolder\runspeedtest.ps1"
