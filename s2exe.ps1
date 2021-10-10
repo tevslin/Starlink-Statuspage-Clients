a36f3c91f2d66969cbced2ea5dd1a1ac796d7bd2
Tom Evslin <tom@evslin.com>
2021-10-10 11:18:22 -0400

    added bootinstaller

--- s2exe.ps1
+++ s2exe.ps1
@@ -4,4 +4,5 @@ invoke-ps2exe $gitfolder\unschedulestarlinkstatus.ps1 $gitfolder\UnscheduleStarl
 invoke-ps2exe $gitfolder\schedulestarlinkstatus.ps1 $gitfolder\ScheduleStarlinkStatus.exe
 invoke-ps2exe $gitfolder\starlinkstatusstarter.ps1 $gitfolder\StarlinkStatusStarter.exe -noconsole
 #invoke-ps2exe $gitfolder\install.ps1 $gitfolder\install.exe
+invoke-ps2exe $gitfolder\starlinkinstaller.ps1 $gitfolder\StarlinkInstaller.exe
 compress-archive -path $gitFolder\*starlink*.exe -DestinationPath $gitFolder\exes.zip -Force
---
+++ starlinkinstaller.ps1
@@ -0,0 +1,31 @@
+﻿function SetUp-Folder{
+    param([string]$FolderName)
+    
+    if ($(Test-Path $FolderName) -eq $true){
+        "$FolderName already exists."
+    }
+    else{
+          try {
+            New-Item -Path $FolderName -ItemType Directory -ErrorAction Stop
+             } 
+           catch {
+            throw "Could not create folder $FolderName!"
+           }
+        }
+        
+       
+
+}
+function DownloadFromRepo{
+    param(
+    [string]$file
+    )
+    $githubfolder="https://raw.githubusercontent.com/tevslin/Starlink-Statuspage-Clients/main"
+    Invoke-WebRequest -Uri $githubfolder/$file -OutFile $starlinkfolder\$file -ErrorAction Stop -Verbose
+}
+$StarlinkFolder="C:\users\$env:USERNAME\documents\StarlinkScripts"
+
+Setup-folder $starlinkfolder
+DownloadFromRepo Install.ps1
+invoke-expression "$StarlinkFolder/Install.ps1"
+
