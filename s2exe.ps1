f385c9668c58e8855b9acf0ed052f8ef33b4f5b3
Tom Evslin <tom@evslin.com>
2021-10-10 10:42:30 -0400

    removed install.exe

--- s2exe.ps1
+++ s2exe.ps1
@@ -3,5 +3,5 @@
 invoke-ps2exe $gitfolder\unschedulestarlinkstatus.ps1 $gitfolder\UnscheduleStarlinkStatus.exe
 invoke-ps2exe $gitfolder\schedulestarlinkstatus.ps1 $gitfolder\ScheduleStarlinkStatus.exe
 invoke-ps2exe $gitfolder\starlinkstatusstarter.ps1 $gitfolder\StarlinkStatusStarter.exe -noconsole
-invoke-ps2exe $gitfolder\install.ps1 $gitfolder\install.exe
-compress-archive -path $gitFolder\*.exe -DestinationPath $gitFolder\exes.zip -Force
+#invoke-ps2exe $gitfolder\install.ps1 $gitfolder\install.exe
+compress-archive -path $gitFolder\*starlink*.exe -DestinationPath $gitFolder\exes.zip -Force
